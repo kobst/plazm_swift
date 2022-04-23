d9b3a011969481d61a28240a11e01d7f3e745b0d
Edward Han <edwrdhn@gmail.com>
2022-04-23 19:42:48 -0400

    Initial Commit

---
+++ plazm_swift/plazm_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  plazm_swiftApp.swift
+//  plazm_swift
+//
+//  Created by Edward Han on 4/23/22.
+//
+
+import SwiftUI
+
+@main
+struct plazm_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
