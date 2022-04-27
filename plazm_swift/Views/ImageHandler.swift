b1d204de85bf8ba7a4bcd79ab9ac3a2ec57f41b1
Edward Han <edwrdhn@gmail.com>
2022-04-27 19:33:35 -0400

    added image handling

---
+++ plazm_swift/Views/ImageHandler.swift
@@ -0,0 +1,8 @@
+//
+//  ImageHandler.swift
+//  plazm_swift
+//
+//  Created by Edward Han on 4/27/22.
+//
+
+import Foundation
--- plazm_swift/Views/ItemView.swift
+++ plazm_swift/Views/ItemView.swift
@@ -17,6 +17,12 @@ struct PostView: View {
     var body: some View {
         VStack {
             HStack{
+//                AsyncImage(url: URL(string: post.listId?[0]?.media!)) { image in
+//                    image.resizable()
+//                } placeholder: {
+//                    Image("plazmLogo").resizable()
+//                }.frame(width: 32, height: 32)
+                
                 Text(post.listId?[0]?.name ?? "").font(.custom("AvenirNext-Medium", size: 16)).foregroundColor(.black).frame(width: 100, height: 100, alignment: .leading)
                 Text(post.business?[0]?.companyName ?? "").font(.custom("AvenirNext-Medium", size: 16)).foregroundColor(.black).frame(width: 100, height: 100, alignment: .trailing)
             }.frame(width: .infinity, height: 100, alignment: .leading)
